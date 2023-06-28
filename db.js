b6a37acd91da851836e45ca0ba99f3512fcd4e55
khardy <khardy.official@gmail.com>
2023-06-28 12:19:43 +0300

    fix

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-);
+const sequelize = new Sequelize({
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
+});
 
 export default sequelize;
